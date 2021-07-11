445c683ec0ec0e3341d37d8cc434a051fb41865c
Juan Jesus <castelock@gmail.com>
2021-07-11 23:54:05 +0200

    Try CNN

--- Activation_functions/main.py
+++ Activation_functions/main.py
@@ -1,1 +1,25 @@
-print("This is the beginning of something great!!")
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import backend as K
+
+# VAriables
+input_shape = (224, 224, 3)
+num_classes = 3
+
+# CNN
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1), activation='relu', input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(1000, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
+
+print("END")
